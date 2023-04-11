dca31df4a6ea0e80c4faca9a7ad9fb88058896ad
Xavier <xavieruxchamp@gmail.com>
2023-04-11 13:58:10 -0700

    Basic JavaScript Projects Assignment
    Pushing the folder containing your “Project1_expressions_alert” folder.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+var Car = 'Ferrari', Train = 'Bombardier', Airplane = 'Cessna' //Defining variables and giving it different string values 
+window.alert(Car);
+
+var Sent1 = "This the beginning of the string", Sent2 = " and this is the end of the string" //Defining variables and giving it different string values 
+
+var Z = (Sent1 + Sent2) //Concatenating 2 variables
+document.write(Z); //Running a document.write alert
